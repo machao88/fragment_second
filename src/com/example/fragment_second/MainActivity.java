1f2a894fb2972f4bf9adbafd32fd8700cf213eae
machao <machao@home.(none)>
2015-05-30 17:34:27 +0800

    [fragment:add,high]Add auto create fragment.

--- src/com/example/fragment_second/MainActivity.java
+++ src/com/example/fragment_second/MainActivity.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,27 +21,23 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		
+		int width = getWindowManager().getDefaultDisplay().getWidth();
+		int height = getWindowManager().getDefaultDisplay().getHeight();
+		
+		Fragment1 fragment1 = new Fragment1();
+		Fragment2 fragment2 = new Fragment2();
+		
+		android.app.FragmentManager fm = getFragmentManager();
+		android.app.FragmentTransaction ft = fm.beginTransaction();
+		
+		if(width > height) {
+			ft.replace(android.R.id.content, fragment1);
 		}
-		return super.onOptionsItemSelected(item);
+		else {
+			ft.replace(android.R.id.content, fragment2);
+		}
+		ft.commit();
 	}
-
 
 }
